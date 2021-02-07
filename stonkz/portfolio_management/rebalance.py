43c96c03ad73c4dee3b664c5394def0cb42325ea
Riley Peterson <rileypeterson21@gmail.com>
2021-02-07 14:09:52 -0800

    Better version

--- stonkz/portfolio_management/rebalance.py
+++ stonkz/portfolio_management/rebalance.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import minimize
 
 
 def no_sell_reallocate(x, t, limit=None):
@@ -33,28 +34,43 @@ def no_sell_reallocate(x, t, limit=None):
     div = x / nan_t
     print(div)
     s = np.nanmax(div) + np.sum(x[t == 0])
+    if limit and limit + s0 < s:
+        s = limit + s0
     i = np.argmax(div) + 1
     b = np.array([s0] + [-j for j in x])
     a = np.eye(len(x) + 1)
     a[0, 1:] = -np.ones(len(x))
     a[1:, 0] = -np.array(t)
-    a = np.delete(a, i, 0)
-    a = np.delete(a, i, 1)
-    b = np.delete(b, i, 0)
+    # a = np.delete(a, i, 0)
+    # a = np.delete(a, i, 1)
+    # b = np.delete(b, i, 0)
     a[0, 1:] = 0
     b[0] = s
     a = np.vstack((a, [1] + (a.shape[1] - 1) * [-1]))
     b = np.append(b, s0)
-    out = np.linalg.lstsq(a, b, rcond=None)
-    out = out[0]
-    print(out)
-    if limit and limit + s0 < out[0]:
-        pcts = out[1:] / sum(out[1:])
-        out = limit * pcts
-        out = np.insert(out, 0, sum(x) + sum(out))
-    out = np.insert(out, i, 0)
+    print(a)
+    print(b)
+    np.linalg.norm(np.dot(a, np.zeros(a.shape[1])) - b)
+    fun = lambda y: np.linalg.norm(np.dot(a, y) - b)
+    out = minimize(
+        fun,
+        np.zeros(a.shape[1]),
+        method="L-BFGS-B",
+        bounds=[(0.0, max(s0 + (limit or 0), s))]
+        + [(0.0, None) for _ in range(a.shape[1] - 1)],
+    )
+    out = out.x
+
+    # out = np.linalg.lstsq(a, b, rcond=None)
+    # out = out[0]
+    # if limit and limit + s0 < out[0]:
+    #     pcts = out[1:] / sum(out[1:])
+    #     out = limit * pcts
+    #     out = np.insert(out, 0, sum(x) + sum(out))
+    # out = np.insert(out, i, 0)
     print(f"New Account Total: ${out[0]:.2f}")
     out = np.delete(out, 0)
+    # out = np.delete(out, -1)
     for i, amt in enumerate(out):
         print(f"x{i} Amount to Buy: ${amt:.2f}")
     new_allocs = list(((x + out) / (sum(x) + sum(out))))
@@ -66,6 +82,10 @@ def no_sell_reallocate(x, t, limit=None):
 
 
 if __name__ == "__main__":
-    x = [5000, 1000, 2500, 100]
-    t = [0.5, 0.15, 0.34, 0.01]
-    print(no_sell_reallocate(x, t))
+    x = [1000, 3000, 2000]
+    t = [0.7, 0.15, 0.15]
+
+    x = [2500, 3000, 2000]
+    t = [0.3, 0.2, 0.5]
+
+    print(no_sell_reallocate(x, t, 4500))
